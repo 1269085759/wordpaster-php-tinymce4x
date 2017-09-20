09b8d99647862e9dbb78a3b9bc2ca5ddb7f9a39e
xfl <xfl@ncmem.com>
2017-09-20 15:37:00 +0800

    load_complete取消隐藏操作

--- WordPaster/js/WordPaster.js
+++ WordPaster/js/WordPaster.js
@@ -598,7 +598,7 @@ function WordPasterManager()
             }
         }
         if (needUpdate) this.need_update();
-        else { $.skygqbox.hide(); }
+        //else { $.skygqbox.hide(); }
     };
     this.recvMessage = function (msg)
 	{
